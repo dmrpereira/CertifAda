c4a2f6719d3cb25e7243d33ce5d9b6df656eecab
David Pereira <dmrpereira@gmail.com>
2013-08-30 16:37:31 +0100

    Added project file

--- main.adb
+++ main.adb
@@ -1,12 +1,8 @@
-with CIC; use CIC;
-with Env; use Env;
-with Checker; use Checker;
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
-   E : Environment := null;
    package PrintBool is new Enumeration_IO(Boolean);
 begin
-   PrintBool.Put(Wf_Empty_Env(E));
+   PrintBool.Put(true);
    null;
 end Main;
